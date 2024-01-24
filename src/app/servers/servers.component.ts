7f4f02535d9821911be98aa35834e61b35a62852
aeherrer71 <aeherrer71@gmail.com>
2024-01-23 21:09:25 -0500

    updated

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+  serverCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+
+  }
+
+  onUpdateServerName(event: any) {  
+    this.serverName = event.target.value; 
+  }
+
+}
