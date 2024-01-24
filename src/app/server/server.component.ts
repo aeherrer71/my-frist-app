fdea9cf72d1616e64d318f1b19ed3adebf15693a
aeherrer71 <aeherrer71@gmail.com>
2024-01-24 12:35:00 -0500

    update

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrl: './server.component.css'
+  styles: `
+  .online {
+    color: white;
+  }
+  `
 })
 export class ServerComponent {
   serverID: number = 10;
